99f027d2c157d81eb49fb62cfc178ccbb114f266
Danit <danishagan@gmail.com>
2021-05-23 21:15:17 +0300

    Create end-of-game screen

--- Assets/Scripts/GameManager1.cs
+++ Assets/Scripts/GameManager1.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager1 : MonoBehaviour
 {
     public Text scoreText;
     public Text playerLives;
+    public Text endScoreText;
+
+    public GameObject gameOverPanel;
 
     public PlayerController ourPlayer;
 
@@ -21,5 +25,24 @@ public class GameManager1 : MonoBehaviour
     {
         scoreText.text = "Score: " + ourPlayer.score;
         playerLives.text = "x " + ourPlayer.lives;
+
+        if (ourPlayer.lives <= 0 && ourPlayer != null)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        Time.timeScale = 0;
+        endScoreText.text = "Your Score: " + ourPlayer.score;
+        gameOverPanel.SetActive(true);
+        Destroy(ourPlayer.gameObject);
+    }
+
+    public void PlayAgain()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
